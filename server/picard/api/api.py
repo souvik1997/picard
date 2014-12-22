f0ff533bc0f61c5c1dd8c60c3724950a4b092acf
Souvik Banerjee <souvik1997@gmail.com>
2014-12-22 11:27:45 -0600

    Change load to engine_load, change table name

--- server/picard/api/api.py
+++ server/picard/api/api.py
@@ -14,35 +14,35 @@ database = os.environ['PICARD_DB']
 
 db = pymysql.connect(host=host, user=username, password=password, db=database)
 cur = db.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS defaulttable (timestamp timestamp, vin varchar(50), rpm decimal, speed decimal, temp integer, load decimal)")
-cur.execute("CREATE UNIQUE INDEX IF NOT EXISTS time_index ON defaulttable(timestamp)")
+cur.execute("CREATE TABLE IF NOT EXISTS picardtable (timestamp timestamp, vehicle_id varchar(50), rpm decimal, speed decimal, temp integer, engine_load decimal)")
+cur.execute("CREATE UNIQUE INDEX IF NOT EXISTS time_index ON picardtable(timestamp)")
 
-# SELECT CAST(avg(timestamp) as integer), avg(speed) from defaulttable group by strftime('%M',datetime(timestamp, 'unixepoch')) order by timestamp;
-# SELECT strftime('%s',(strftime('%Y-%m-%d %H:%M',datetime(timestamp, 'unixepoch')))) as 'Timestamp', avg(speed) from defaulttable group by 1;
+# SELECT CAST(avg(timestamp) as integer), avg(speed) from picardtable group by strftime('%M',datetime(timestamp, 'unixepoch')) order by timestamp;
+# SELECT strftime('%s',(strftime('%Y-%m-%d %H:%M',datetime(timestamp, 'unixepoch')))) as 'Timestamp', avg(speed) from picardtable group by 1;
 
 print(prefix)
 
 def validate(field):
-	if field not in ['vin','timestamp','rpm','speed','temp','load']:
+	if field not in ['vehicle_id','timestamp','rpm','speed','temp','engine_load']:
 		return False
 	else:
 		return True
 
-@route(prefix+'get/<vin>/<timestamp>/<field>')
+@route(prefix+'get/<vehicle_id>/<timestamp>/<field>')
 def get_data_timestamp(vin,timestamp,field):
 	if validate(field):
 		try:
-			json.dumps(cur.execute("SELECT timestamp,"+field+" FROM defaulttable WHERE vin = ? and timestamp = ? ", (vin, timestamp)).fetchall())
+			json.dumps(cur.execute("SELECT timestamp,"+field+" FROM picardtable WHERE vehicle_id = ? and timestamp = ? ", (vin, timestamp)).fetchall())
 		except:
 			return 'error'
 	else:
 		return 'error'
 
-@route(prefix+'get/<vin>/<timestamp_from>/<timestamp_to>/<field>')
+@route(prefix+'get/<vehicle_id>/<timestamp_from>/<timestamp_to>/<field>')
 def get_data_timestamp_range(vin,timestamp_from, timestamp_to, field):
 	if validate(field):
 		try:
-			return json.dumps(cur.execute("SELECT timestamp,"+field+" FROM defaulttable WHERE vin = ? and timestamp >= ? AND timestamp < ? ", (vin, timestamp_from, timestamp_to)).fetchall())
+			return json.dumps(cur.execute("SELECT timestamp,"+field+" FROM picardtable WHERE vehicle_id = ? and timestamp >= ? AND timestamp < ? ", (vin, timestamp_from, timestamp_to)).fetchall())
 		except:
 			return 'error'
 	else:
@@ -53,7 +53,7 @@ def get_data_timestamp_range(vin,timestamp_from, timestamp_to, field):
 def get_data(field):
 	if validate(field):
 		try:
-			return "["+json.dumps(cur.execute("SELECT DISTINCT "+field+" FROM defaulttable ").fetchall()).replace("[","").replace("]","")+"]"
+			return "["+json.dumps(cur.execute("SELECT DISTINCT "+field+" FROM picardtable ").fetchall()).replace("[","").replace("]","")+"]"
 		except:
 			return 'error'
 	else:
@@ -62,7 +62,7 @@ def get_data(field):
 @post(prefix+'upload')
 def upload_data():
 	initial_time   = request.forms.get('initial_time')
-	vin   = request.forms.get('vin')
+	vehicle_id   = request.forms.get('vehicle_id')
 	upload     = request.files.get('upload')
 	name, ext = os.path.splitext(upload.filename)
 	if ext not in ('.csv'):
@@ -70,8 +70,8 @@ def upload_data():
 	upload.save("/tmp/temp.csv", overwrite=True)
 	with open("/tmp/temp.csv","r") as csvfile:
 		dr = csv.DictReader(csvfile)
-		to_db = [(int(1000*((float(i['time'])+float(initial_time)))), vin, float(i['rpm']), float(i['speed']), int(i['temp']), float(i['load'])) for i in dr]
-		cur.executemany("INSERT INTO defaulttable (timestamp, vin, rpm, speed, temp, load) VALUES (?, ?, ?, ?, ?, ?);", to_db)
+		to_db = [(int(1000*((float(i['time'])+float(initial_time)))), vehicle_id, float(i['rpm']), float(i['speed']), int(i['temp']), float(i['load'])) for i in dr]
+		cur.executemany("INSERT INTO picardtable (timestamp, vin, rpm, speed, temp, engine_load) VALUES (?, ?, ?, ?, ?, ?);", to_db)
 	db.commit()
 	return 'OK'
 
